858847d0fe2823e1b65881414fed8b405d4925c6
Jobaben <usha.badr@gmail.com>
2025-11-02 16:04:28 +0100

    Refine worker shutdown draining logic

--- apps/worker/cmd/worker/main.go
+++ apps/worker/cmd/worker/main.go
@@ -180,31 +180,19 @@ func (p *ingestionProcessor) Run(ctx context.Context) {
 
 func (p *ingestionProcessor) processJobs(ctx context.Context, jobs <-chan *queuepkg.IngestionJob) {
 	drainCtx := context.WithoutCancel(ctx)
-	jobCtx := ctx
 
 	for {
-		if jobCtx == ctx && ctx.Err() != nil {
-			jobCtx = drainCtx
-		}
-
 		select {
 		case job, ok := <-jobs:
 			if !ok {
 				return
 			}
-			p.handleJob(jobCtx, job)
+			p.handleJob(ctx, job)
 		case <-ctx.Done():
-			jobCtx = drainCtx
-
-			select {
-			case job, ok := <-jobs:
-				if !ok {
-					return
-				}
-				p.handleJob(jobCtx, job)
-			default:
-				return
+			for job := range jobs {
+				p.handleJob(drainCtx, job)
 			}
+			return
 		}
 	}
 }
